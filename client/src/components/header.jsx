f578e8d1883337133202aaf6654e46145abeb4a1
SidhuDilbagh <dilbaghsidhu25@gmail.com>
2024-09-04 11:44:22 +0530

    update header

--- client/src/components/header.jsx
+++ client/src/components/header.jsx
@@ -1,9 +1,11 @@
-import { Button, Navbar, TextInput } from "flowbite-react";
+import { Avatar, Button, Dropdown, DropdownHeader, Navbar, TextInput } from "flowbite-react";
 import { Link, NavLink, useLocation } from "react-router-dom";
 import { AiOutlineSearch, AiFillMoon } from "react-icons/ai";
+import { useSelector } from "react-redux";
 
 export default function Header() {
-    const location=useLocation().pathname;
+  const location = useLocation().pathname;
+  const { currentUser } = useSelector((state) => state.user);
   return (
     <Navbar className="border-y-2 bg-orange-50 dark:bg-black">
       <Link
@@ -33,23 +35,69 @@ export default function Header() {
             color="rose"
           />
         </button>
-        <Link
+        {currentUser ? (
+          <Dropdown
+            arrowIcon={false}
+            inline
+            label={
+              <Avatar 
+              img={currentUser.profilePicture}
+              size='xs'
+              rounded
+              />
+            }
+          >
+            <Dropdown.Header>
+              <span className="block text-sm" >@{currentUser.username}</span>
+              <span className="block text-sm truncate" >{currentUser.email}</span>
+            </Dropdown.Header>
+            <Dropdown.Header>
+              <Link to={'/dashboard'}>
+              <Dropdown.Item>
+                profile
+              </Dropdown.Item>
+            </Link>
+            <Dropdown.Divider />
+            <Dropdown.Item>SignOut</Dropdown.Item>
+            </Dropdown.Header>
+          </Dropdown>
+        ) : (
+          <Link
           to="/signIn"
           className="self-center whitespace-nowrap text-sm  px-2  bg-gradient-to-tl from-orange-400 to-amber-200 hover:bg-rose-950 rounded-full text-rose-950 hover:text-orange-100"
-        >
-          <button>SignIn</button>
-        </Link>
+          >
+            <button>SignIn</button>
+          </Link>
+        )}
+
         <Navbar.Toggle />
       </div>
       <Navbar.Collapse>
-        <Navbar.Link active={location === "/" } className={`text-red-950 ${location === "/" ? "bg-orange-100 lg:bg-none " : ""}`}
-          as={'div'}>
+        <Navbar.Link
+          active={location === "/"}
+          className={`text-red-950 ${
+            location === "/" ? "bg-orange-100 lg:bg-none " : ""
+          }`}
+          as={"div"}
+        >
           <Link to="/">home</Link>
         </Navbar.Link>
-        <Navbar.Link active={location === "/about" } className={`text-red-950 ${location === "/about" ? "bg-orange-100 lg:bg-none " : ""}`} as={'div'}>
-          <Link to="/about" >about</Link>
+        <Navbar.Link
+          active={location === "/about"}
+          className={`text-red-950 ${
+            location === "/about" ? "bg-orange-100 lg:bg-none " : ""
+          }`}
+          as={"div"}
+        >
+          <Link to="/about">about</Link>
         </Navbar.Link>
-        <Navbar.Link active={location === "/projects" } className={`text-red-950 ${location === "/projects" ? "bg-orange-100 lg:bg-none " : ""}`} as={'div'}>
+        <Navbar.Link
+          active={location === "/projects"}
+          className={`text-red-950 ${
+            location === "/projects" ? "bg-orange-100 lg:bg-none " : ""
+          }`}
+          as={"div"}
+        >
           <Link to="/projects">projects</Link>
         </Navbar.Link>
       </Navbar.Collapse>
